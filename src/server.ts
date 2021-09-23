0e643b1d284071d7a82f5c00403dd8100df54e75
scolocho <scolocho@popoyan.com>
2021-09-22 19:25:13 -0600

    saved data

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,14 @@ import  morgan from "morgan";
 
 const app = express();
 const server = http.createServer(app);
+
 app.use(express.json());
 app.use(router);
 app.use(morgan("dev"));
 
 app.set( "ipaddr", "127.0.0.1" ); 
 app.set( "port",  3000);
+
+server.address()
 
 server.listen( 3000, () => {console.log('server is running on port http://localhost:'+`${3000}`,) });
